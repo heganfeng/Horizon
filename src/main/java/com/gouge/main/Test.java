360fd72edf639efeaaedd5a18cb3db3948c07613
heganfeng <heganfeng@sina.cn>
2018-08-26 14:48:52 +0800

    修改测试方法

--- src/main/java/com/gouge/main/Test.java
+++ src/main/java/com/gouge/main/Test.java
@@ -11,6 +11,6 @@ import javax.swing.*;
  */
 public class Test extends JFrame {
     public static void main(String[] args) {
-        System.out.println(11);
+        System.out.println(11111);
     }
 }
